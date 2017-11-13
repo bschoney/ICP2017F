dc2af87408979c94cdee5b62af150465d594e7e2
Barrett Schonefeld <bschoney@utexas.edu>
2017-11-13 09:57:26 -0600

    Update evalTempVec.m

--- Homework/4/1/evalTempVec.m
+++ Homework/4/1/evalTempVec.m
@@ -2,7 +2,7 @@
 entryNum = input('How many temperatures would you like to enter? ');
 
 for i = 1:entryNum
-    TempVec = input('Enter the temperatures you would like to convert (one at a time): ');
+    TempVec(i) = input('Enter the temperatures you would like to convert (one at a time): ');
 end
 
 ConvertType = input('Enter F2C for Fahrenheit to Celsius or C2F for Celsius to Fahrenheit: ', 's');
